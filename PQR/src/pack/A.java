de1cef4c056118a55ca0eb90659e121aef67517b
Dargi Imthiyaz <imtiyazbasha132@gmail.com>
2019-02-12 13:55:55 +0530

    added class and project

---
+++ PQR/src/pack/A.java
@@ -0,0 +1,16 @@
+package pack;
+
+import org.testng.annotations.Test;
+
+public class A {
+
+	@Test()
+	public void x() {
+		System.out.println("hi x");
+	}
+
+	@Test()
+	public void y() {
+		System.out.println("hi y");
+	}
+}
