793f4eaf60da82e02a33a81d1229ff69f70e0bdf
Dargi Imthiyaz <imtiyazbasha132@gmail.com>
2019-02-12 16:48:34 +0530

    update

--- Amazon/src/pack1/A1.java
+++ Amazon/src/pack1/A1.java
@@ -4,7 +4,11 @@ public class A1 {
 
 	public void LaunchBrowser()
 	{
-		
+		System.out.println("hi");
+	}
+	public void m1()
+	{
+		System.out.println("hi");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
